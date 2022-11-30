a45475c3f72a1a03569a040864922de8b291d41b
vinicius <viniciusvwh1441@gmail.com>
2022-11-30 05:28:30 -0300

    feat: Login

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { UserProvider } from '@auth0/nextjs-auth0'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+}
